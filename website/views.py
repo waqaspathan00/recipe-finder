647957329f5c336af0b093cb4bdf4b5c03061df6
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-12-18 03:11:02 -0500

    wrote logic for displaying the ingredients for requested food

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 """ this file manages our different views """
-
+from pprint import pprint
 from flask import Blueprint, render_template, request
 import requests
+import json
 
 views = Blueprint("views", __name__)
 SPOONACULAR_KEY = "a8529c104d8749b4a19488d0fd654353"
@@ -20,8 +21,22 @@ def send_recipe():
         # get the name and id of a food relevant to what the user searched for
         url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={SPOONACULAR_KEY}&query={food}&instructionsRequired=true"
         r = requests.get(url)
-        food_data = eval(r.text)["results"]
+        # food_data = eval(r.text)["results"]
+        food_data = json.loads(r.text)["results"]
         food_name = food_data[0]["title"]
+        food_id = food_data[0]["id"]
 
-        return render_template("home.html", food_name=food_name)
+        # get the ingredient data for the extracted recipe
+        url = f"https://api.spoonacular.com/recipes/{food_id}/ingredientWidget.json?apiKey={SPOONACULAR_KEY}"
+        r = requests.get(url)
+        ingredient_data = json.loads(r.text)["ingredients"]
+
+        # using ingredient data, we want the name and amount of each ingredient
+        ingredients = []
+        for ingredient in ingredient_data:
+            name = ingredient["name"]
+            amount = ingredient["amount"]["us"]
+            ingredients.append([name, amount])
+
+        return render_template("home.html", food_name=food_name, ingredients=ingredients)
     return render_template("home.html")
