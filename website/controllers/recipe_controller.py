f0d8276a1fb212aadcd01218f2c6b9bee74dbfa7
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-12-23 20:02:06 -0500

    refactored code to use new RecipeHandler object

--- website/controllers/recipe_controller.py
+++ website/controllers/recipe_controller.py
@@ -1,6 +1,8 @@
-""" controlls post and get requests relating to recipes """
+""" controls post and get requests relating to recipes """
 
 from flask import render_template, request
+from helpers.csv_handler import Writer
+from helpers.recipe import RecipeHandler
 import requests
 import json
 
@@ -25,28 +27,19 @@ class RecipeController:
             url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={SPOONACULAR_KEY}&query={food}&instructionsRequired=true"
             r = requests.get(url)
             food_data = json.loads(r.text)["results"]
-            print(food_data)
-            food_name = food_data[0]["title"]
-            food_id = food_data[0]["id"]
+            first_food_name = food_data[0]["title"]
+            first_food_id = food_data[0]["id"]
 
             # get the ingredient data for the extracted recipe
             url = f"https://api.spoonacular.com/recipes/{food_id}/ingredientWidget.json?apiKey={SPOONACULAR_KEY}"
             r = requests.get(url)
             ingredient_data = json.loads(r.text)["ingredients"]
 
-            # using ingredient data, we want the name and amount of each ingredient
-            ingredients = []
-            for ingredient in ingredient_data:
-                name = ingredient["name"]
-                amount = ingredient["amount"]["us"]["value"]
-                unit = ingredient["amount"]["us"]["unit"]
-                ingredients.append({"name": name, "amount": amount, "unit": unit})
+            ingredients = RecipeHandler.get_ingredients(first_food_id)
 
-            url = f"https://api.spoonacular.com/recipes/{food_id}/analyzedInstructions?apiKey={SPOONACULAR_KEY}"
-            r = requests.get(url)
-            steps = json.loads(r.text)[0]["steps"]
+            steps = RecipeHandler.get_steps(first_food_id)
 
-            return render_template("home.html", food_name=food_name, ingredients=ingredients, steps=steps)
+            return render_template("home.html", first_food_name=first_food_name, ingredients=ingredients, steps=steps)
         return render_template("home.html")
 
     @staticmethod
