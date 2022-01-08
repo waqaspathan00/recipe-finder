9e2cd69e75680ba330f339e3e0dd82a0a09c76c9
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2022-01-07 23:16:56 -0500

    refactored code using create_objs to create Food and Ingredient

--- helpers/recipe.py
+++ helpers/recipe.py
@@ -15,14 +15,9 @@ class Food(dict):
 
         super().__init__(self, id=id, name=name, image=image)
 
-    @classmethod
-    def create_foods(cls, food_data):
-        """ create a list of all food objects """
-
-        return [cls(data) for data in food_data]
 
 class Ingredient(dict):
-    """ by inheriting from dict, Food objects become automatically serializable for JSON formatting """
+    """ by inheriting from dict, Ingredient objects become automatically serializable for JSON formatting """
 
     def __init__(self, data):
         name = data["name"]
@@ -31,13 +26,17 @@ class Ingredient(dict):
 
         super(Ingredient, self).__init__(name=name, amount=amount, unit=unit)
 
+def create_objs(obj, data):
+    """ create a list of desired objects """
+    return [obj(row) for row in data]
+
 def get_foods_by_name(food):
     """ get a list of all the returned foods using user provided food name """
 
     url = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={SPOONACULAR_KEY}&query={food}&instructionsRequired=true&number=100"
     r = requests.get(url)
     food_data = json.loads(r.text)["results"]  # convert response to readable dictionary
-    foods = Food.create_foods(food_data)
+    foods = create_objs(Food, food_data)
 
     return foods
 
@@ -51,7 +50,7 @@ def get_foods_by_ingredients(ingredients):
     url = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={SPOONACULAR_KEY}&ingredients={ingredient_str}"
     r = requests.get(url)
     food_data = json.loads(r.text)
-    foods = Food.create_foods(food_data)
+    foods = create_objs(Food, food_data)
 
     return foods
 
@@ -61,11 +60,7 @@ def get_ingredients(food_id):
     url = f"https://api.spoonacular.com/recipes/{food_id}/ingredientWidget.json?apiKey={SPOONACULAR_KEY}"
     r = requests.get(url)  # perform a get request on the url
     ingredient_data = json.loads(r.text)["ingredients"]
-
-    # create a list of the desired foods Ingredient objects, each one containing ingredient name, amount, and unit
-    ingredients = []
-    for data_row in ingredient_data:
-        ingredients.append(Ingredient(data_row))
+    ingredients = create_objs(Ingredient, ingredient_data)
 
     return ingredients
 
